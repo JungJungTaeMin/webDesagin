aed4acc8a5ada8a478c37cb14949e09afb1b99f4
JungJungTaeMin <jtm45890821@gmail.com>
2024-11-08 18:38:59 +0900

    올리기용

---
+++ js/nike.js
@@ -0,0 +1,15 @@
+// let는 재선언 불가 재할당 가능 
+// const  재선언 불가 재할당 불가능
+// var 재선언 가능 재할당 가능
+
+
+const $ = (element) => document.querySelector(element);
+
+
+const $img_box = $('.img_box') 
+
+console.log($img_box.children);
+
+function slide(){
+    
+}
